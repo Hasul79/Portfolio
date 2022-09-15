94b63099691e68c359511d0873786d8d8a970573
Hasul79 <hatuk2@mail.ru>
2022-09-15 19:17:37 +0400

    1 commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+import './App.css';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import About from './components/about/About';
+import Experience from './components/experience/Experience';
+import Service from './components/service/Service';
+import Portfolio from './components/portfolio/Portfolio';
+import Testimonials from './components/testimonials/Testimonials';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+
+function App() {
+  return (
+    <>
+     <Header />
+     <Nav />
+     <About />
+     <Experience />
+     <Service />
+     <Portfolio />
+     <Testimonials />
+     <Contact />
+     <Footer />
+
+    </>
+  );
+}
+
+export default App;
